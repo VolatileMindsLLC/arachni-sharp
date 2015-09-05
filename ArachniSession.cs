69c537406c757a2597c0fc1ae10b4443b70caf3a
Brandon Perry <bperry.volatile@gmail.com>
2015-09-04 20:00:15 -0500

    add stuff

---
+++ ArachniSession.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace arachnisharp
+{
+	public class ArachniSession
+	{
+		public ArachniSession ()
+		{
+		}
+	}
+}
+
---
+++ example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace example
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
